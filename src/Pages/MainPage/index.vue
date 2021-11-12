aa0dd46a6f3980fe1555d1d0ffcfd7bba4329f0b
José <perezg.josesal@gmail.com>
2021-11-12 01:42:39 -0600

    Feat - Textos de descripcion agregados

--- src/Pages/MainPage/index.vue
+++ src/Pages/MainPage/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="tab-inicio">
-    <JSText>Vamos con todo para LoL Hate</JSText>
+    <JSText variant="subtitle">
+      ¿Qué es LoL Hate?
+    </JSText>
+    <JSText variant="description">
+      Debido a la falta de un sistema de registro de las experiencias
+      entre los jugadores en partidas de League of Legends surge LoL Hate.
+      Un lugar donde se puede comentar, registrar y revisar experiencias
+      propias o de otros jugadores a través de su nombre de invocador.
+    </JSText>
+    <JSText variant="description">
+      No solo se busca generar registros, si no ofrecer un espacio donde
+      cualquier invocador puedes desahogar cualquier sentimiento proveniente de la conclusión
+      de una partida de League of Legends, sin importar si es una victoria o derrota.
+    </JSText>
   </div>
 </template>
 <script>
