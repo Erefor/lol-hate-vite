082b42b308c64e44347f24b082fcbbbf1b7dd863
José <chocomouuu@gmail.com>
2021-11-13 14:52:13 -0600

    No recuerdo qué hice

--- src/Boot/Firebase.js
+++ src/Boot/Firebase.js
@@ -14,4 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 const database = getDatabase()
 
-export default  { app, database }
+export  { app, database }
--- src/Pages/ReportPage/index.vue
+++ src/Pages/ReportPage/index.vue
@@ -6,14 +6,24 @@
         v-model="summonerName"
         :rules="[value => value.trim() !== '' || 'Agrega un nombre valido']"
       />
-      <button>Buscar</button>
+      <button @click="getSummonerData">
+        Buscar
+      </button>
+      <teleport>
+        <JSText>
+          Hola, puerco
+        </JSText>
+      </teleport>
     </div>
+    <JSText v-if="summonerData">
+      {{ summonerData.name }}
+    </JSText>
   </div>
 </template>
 
 <script>
 import { ref } from 'vue'
-import { database } from 'src/Boot/Firebase'
+import { database } from '../../Boot/Firebase'
 import { ref as refFirebase, get, child } from 'firebase/database'
 import JSInput from '../../components/Molecules/JSInput.vue'
 export default {
@@ -23,15 +33,26 @@ export default {
     },
     setup() {
         const summonerName = ref('')
-        // eslint-disable-next-line no-unused-vars
         const apiKey = ref('')
+        const summonerData = ref(null)
         async function getApiKey() {
-            const key = get(await child(refFirebase(database), '/apiKey'))
-            console.log(key)
+            const key = await get(await child(refFirebase(database), '/apiKey'))
+            apiKey.value = key.val()
+        }
+        async function getSummonerData() {
+            try {
+                const data = await fetch(`https://la1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName.value}?api_key=${apiKey.value}`)
+                summonerData.value = await data.json()
+                console.log(summonerData.value)
+            } catch (e) {
+                console.log(e)
+            }
         }
         getApiKey()
         return {
             summonerName,
+            summonerData,
+            getSummonerData,
         }
     }
 }
